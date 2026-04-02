2078b17a764a89f551c4bbc2fd9bfbe7c8a1310f
CHETHAN BADIGER <chetandvg2004@gmail.com>
2026-04-02 22:27:50 +0530

    Refactor myAtoi method by removing comments
    Removed comments and adjusted whitespace in myAtoi method.

--- LeetCode/string to integer.py
+++ LeetCode/string to integer.py
@@ -1,22 +1,16 @@
 class Solution(object):
     def myAtoi(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # Step 1: Trim leading whitespace
+        
         s = s.lstrip()
         if not s:
             return 0
 
-        # Step 2: Handle optional sign
         sign = 1
         if s[0] in ['-', '+']:
             if s[0] == '-':
                 sign = -1
             s = s[1:]
 
-        # Step 3: Convert digits
         result = 0
         for char in s:
             if char.isdigit():
@@ -26,7 +20,6 @@ class Solution(object):
 
         result *= sign
 
-        # Step 4: Clamp to 32-bit signed integer range
         INT_MIN, INT_MAX = -2**31, 2**31 - 1
         if result < INT_MIN:
             return INT_MIN
